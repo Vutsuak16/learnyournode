ae85a956fa0a3c5b729d86cd9f3f3ae4c7896434
kaustuv deolal <vutsuak96@gmail.com>
2016-03-30 00:08:31 +0530

    creating http post server

---
+++ uppercasers.js
@@ -0,0 +1,18 @@
+var map=require('through2-map');
+var http=require('http');
+
+const PORT=process.argv[2]; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
